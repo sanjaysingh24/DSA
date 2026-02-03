1e8ac18fb7c08f788ff4107eb7fd28e7e98230ca
sanjaysingh24 <sanjubora84@gmail.com>
2026-02-03 21:52:37 +0530

    update

--- Array methods/Phase-2.js
+++ Array methods/Phase-2.js
@@ -116,3 +116,15 @@ function productgiven(n){
     console.log(prod);
 }
 // productgiven(123)
+
+function countnumberindigit(n){
+    let count=0;
+    while(n>0){
+        let lastdigit = n%10;
+        count++;
+        n=Math.floor(n/10);
+    }
+    console.log(count);
+    
+}
+countnumberindigit(2343)
