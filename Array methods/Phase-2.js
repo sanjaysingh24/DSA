e1f2c6531c01d12c3caa32c55beb72b4dd123b23
sanjaysingh24 <sanjubora84@gmail.com>
2026-02-08 11:29:32 +0530

    centerd pyramid

--- Array methods/Phase-2.js
+++ Array methods/Phase-2.js
@@ -519,4 +519,21 @@ function oddstarpatter(n){
       console.log(p);
     }
 }
-oddstarpatter(10);
+// oddstarpatter(10);
+
+
+function centeredpyramid(n){
+    for(let i=0;i<n;i++){
+        let p="";
+        // for spacing
+        for(let j=0;j<n-i-1;j++){
+           p+=" "
+        }
+        //add start
+        for(let k=0;k<2*i+1;k++){
+            p+="*"
+        }
+     console.log(p);
+    }
+}
+centeredpyramid(10);
