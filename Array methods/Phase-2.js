f82703056745c817acd43af435acec433b4ddd49
sanjay <89508377+sanjaysingh24@users.noreply.github.com>
2026-02-10 21:53:30 +0530

    Remove extra code
    Removed introductory comments about looping and iteration.

--- Array methods/Phase-2.js
+++ Array methods/Phase-2.js
@@ -1,8 +1,4 @@
-//looping  & patters(iteration & flow)
-// goal master loops, iteration and dry-run thinking
-// Topics covered: for, while, nested loops, break/continue, mathematical series
-//Target-question 40-50
-//level-1 basic looping (introductory)
+
 //1- print numbers from 1 to 10
 function printnumber(){
     let i=1;
