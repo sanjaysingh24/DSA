2c947a98a3dc79029e80b81d87d7d16f0e7c92e2
sanjaysingh24 <sanjubora84@gmail.com>
2026-06-24 21:29:20 +0530

    update

--- Array methods/ArrayInsertion.js
+++ Array methods/ArrayInsertion.js
@@ -1,18 +1,29 @@
 // first  insert at specific position 
-let arr = [1,2,4,3,5]
+let arr = [1,2,4,3,5,6]
 function insertionatspecific(arr,pos,element){
     for(let i =arr.length-1;i>=pos-1;i--){
         
-         arr[i+1]=arr[i]
+         arr[i+1]=arr[i];
          
-         
-         
-     
-
     }
-arr[pos-1]=element;
+    arr[pos-1]=element;
     console.log(arr)
 
 }
 
-insertionatspecific(arr,2,10)
+
+
+// insertionatspecific(arr,2,10)
+
+//insertion at the mid
+function insertioninmid(arr,element){
+    let mid = Math.floor(arr.length/2);
+    for(let i=arr.length-1;i>=mid-1;i--){
+        arr[i+1]=arr[i]
+
+    }
+    arr[mid-1]=element
+console.log(arr)
+}
+
+insertioninmid(arr,30)
