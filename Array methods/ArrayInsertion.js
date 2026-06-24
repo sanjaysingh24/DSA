a8c4f2a325a72478abcbf497a720578764b5f1fa
sanjaysingh24 <sanjubora84@gmail.com>
2026-06-24 21:18:38 +0530

    update

--- Array methods/ArrayInsertion.js
+++ Array methods/ArrayInsertion.js
@@ -2,7 +2,7 @@
 let arr = [1,2,4,3,5]
 function insertionatspecific(arr,pos,element){
     for(let i =arr.length-1;i>=pos-1;i--){
-        //  console.log(arr[i])   
+        
          arr[i+1]=arr[i]
          
          
