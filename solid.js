21ecb2953b53a890224202ab51183a2b0693ebcc
sanjaysingh24 <sanjubora84@gmail.com>
2026-07-03 21:42:19 +0530

    update

---
+++ solid.js
@@ -0,0 +1,27 @@
+class User{
+    saveUser(){
+
+    console.log("save user")
+    }
+    sendEmail(){
+    console.log("Sending email...");
+    }
+    generateReport(){
+        console.log("Generating report...");
+    }
+}
+
+// above we introduce  one single class with multiple methods which is not good practice 
+
+
+class saveUser{
+    saveuser(){
+        console.log("save user")
+    }
+}
+s
+class sendEmail{
+    sendEmail(){
+        console.log("Sending email...");
+    }
+}
