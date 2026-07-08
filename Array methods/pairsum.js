10aabc5016ec134f2f48e2962dbb751a485a7786
sanjaysingh24 <sanjubora84@gmail.com>
2026-07-08 21:50:33 +0530

    update

---
+++ Array methods/pairsum.js
@@ -0,0 +1,30 @@
+// in this i have to find the pair of given sum for example if the sum is 10 than i have to search the  pair of the sum from the array
+
+let arr = [6,3,8,10,16,7,5,2,9,14]
+function findpairsum(arr,k){
+    for(let i=0;i<arr.length;i++){
+        for(let j=i+1;j<arr.length;j++){
+            if(arr[i]+arr[j]==k){
+                console.log(arr[i],arr[j])
+            }
+        }
+    }
+}
+// findpairsum(arr,10)
+
+
+//using hash 
+
+let ex =[6,3,8,10,16,7,5,2,9,14]
+function findPair(arr,sum){
+    let hash  ={};
+    for(let i=0;i<arr.length;i++){
+        
+        let need = sum-arr[i]
+        if(hash[need]){
+            console.log(need,arr[i])
+        }
+        hash[arr[i]]=true
+    }
+}
+findPair(arr,10)
