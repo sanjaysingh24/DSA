b11337284d53b56a21096748196221504ce70bd7
sanjaysingh24 <sanjubora84@gmail.com>
2026-07-20 18:49:14 +0530

    update

---
+++ String/duplicate.js
@@ -0,0 +1,21 @@
+//finding duplicate charachter in one single string
+const word = "finding"
+function findduplicate(word){
+   let count={}
+   for(let char of word){
+     if(count[char]){
+        count[char]++;
+     }
+     else{
+        count[char]=1;
+     }
+   }
+console.log(count);
+//    for(let char in count){
+//     if(count[char]>1){
+//         console.log( `${char} -> ${count[char]}` )
+//     }
+//    }
+}
+
+findduplicate(word)
